e0c11a37a622459e51452fed5ca7b5b359211b51
COMPU-S028\daw1 <example>
2026-05-06 11:30:19 +0200

    Correccion  del trigger y primera consulta hecha

--- Documentación SQL/triggers.sql
+++ Documentación SQL/triggers.sql
@@ -1,7 +1,15 @@
+USE todotabla;
+
 -- Human write trigger
 
 DELIMITER $$
-CREATE TRIGGER guardarHistorial AFTER INSERT ON estado FOR EACH ROW 
+CREATE TRIGGER guardarHistorial AFTER INSERT ON tarea FOR EACH ROW 
+BEGIN 
+	INSERT INTO historial_tareas SELECT NEW.estado, NEW.id, NOW();
+END $$
+
+DELIMITER $$
+CREATE TRIGGER guardarHistorial AFTER UPDATE ON tarea FOR EACH ROW 
 BEGIN 
 	INSERT INTO historial_tareas SELECT NEW.estado, NEW.id, NOW();
 END $$
