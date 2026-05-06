f22f5d6de8d55dfbdc3eb12a39c3946980f14f55
COMPU-S028\daw1 <example>
2026-05-06 09:41:03 +0200

    Añadido cargar la conguracion de un archivo. Este se encuentra en los resources del package

--- UX/TodoTabla/src/main/java/com/decroly/todotabla/model/sql/BBDD.java
+++ UX/TodoTabla/src/main/java/com/decroly/todotabla/model/sql/BBDD.java
@@ -1,18 +1,38 @@
 package com.decroly.todotabla.model.sql;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 public class BBDD {
     private static Connection conexion = null;
 
-    private static Connection getConnection() throws SQLException, ClassNotFoundException {
+    private static String url;
+    private static String user;
+    private static String password;
+
+    private static final URL configURL = BBDD.class.getResource("config.dat");
+
+    private static Connection getConnection() throws Exception {
         Class.forName("com.mysql.jdbc");
 
-        if (conexion == null) {
-            conexion = DriverManager.getConnection("", "", "");
+        if (configURL != null) {
+
+            FileReader configFile = new FileReader(new File(configURL.toURI()));
+            BufferedReader configReader = new BufferedReader(configFile);
+
+            url = "jdbc:mysql:" + "//" + configReader.readLine() + "/" + configReader.readLine();
+            user = configReader.readLine();
+            password = configReader.readLine();
+
+            if (conexion == null) {
+                conexion = DriverManager.getConnection(url, user, password);
+            }
         }
         return conexion;
     }
@@ -21,12 +41,12 @@ public class BBDD {
         try {
             if (conexion != null) {
                 if (restart || conexion.isValid(7200)) {
-                    conexion = DriverManager.getConnection("", "", "");
+                    conexion = DriverManager.getConnection(url, user, password);
                 }
             } else {
                 return getConnection();
             }
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
