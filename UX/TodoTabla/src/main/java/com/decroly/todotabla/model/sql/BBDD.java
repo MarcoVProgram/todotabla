77b77e4297d3aae65d4e2aa06d1e873d105495c1
COMPU-S028\daw1 <example>
2026-05-06 08:35:17 +0200

    Funciones necesarias para obtener una conexion y que esta este activa durante toda la ejecucion del programa en model.sql

--- UX/TodoTabla/src/main/java/com/decroly/todotabla/model/sql/BBDD.java
+++ UX/TodoTabla/src/main/java/com/decroly/todotabla/model/sql/BBDD.java
@@ -3,8 +3,33 @@ package com.decroly.todotabla.model.sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class BBDD {
+    private static Connection conexion = null;
 
-    // Connection conexion = DriverManager.getConnection("", "", "")
+    private static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc");
+
+        if (conexion == null) {
+            conexion = DriverManager.getConnection("", "", "");
+        }
+        return conexion;
+    }
+
+    public static Connection getConnection(boolean restart)  {
+        try {
+            if (conexion != null) {
+                if (restart || conexion.isValid(7200)) {
+                    conexion = DriverManager.getConnection("", "", "");
+                }
+            } else {
+                return getConnection();
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return conexion;
+    }
 }
