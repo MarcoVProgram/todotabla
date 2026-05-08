e2d92a5bbc0938f48546420d9332214834ff04a3
Joaquin Ottonello <joaquinottonello@protonmail.com>
2026-05-08 08:48:44 +0200

    no message

--- UX/TodoTabla/src/main/java/com/decroly/todotabla/model/Asignacion.java
+++ UX/TodoTabla/src/main/java/com/decroly/todotabla/model/Asignacion.java
@@ -3,15 +3,22 @@ package com.decroly.todotabla.model;
 import java.time.LocalDate;
 
 public class Asignacion {
-    private static int cont;
+
     private int id;
     private Miembro idMiembro;
     private Tarea idTarea;
     private LocalDate fechaAsignacion;
     private LocalDate fechaFin;
 
+    public Asignacion(int id, Miembro idMiembro, Tarea idTarea, LocalDate fechaAsignacion, LocalDate fechaFin) {
+        this.id = id;
+        this.idMiembro = idMiembro;
+        this.idTarea = idTarea;
+        this.fechaAsignacion = fechaAsignacion;
+        this.fechaFin = fechaFin;
+    }
+
     public Asignacion(Miembro idMiembro, Tarea idTarea, LocalDate fechaAsignacion, LocalDate fechaFin) {
-        this.id = cont++;
         this.idMiembro = idMiembro;
         this.idTarea = idTarea;
         this.fechaAsignacion = fechaAsignacion;
