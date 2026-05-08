5287238974f8a16e7de05ceab721a3572f1fe2ae
Joaquin Ottonello <joaquinottonello@protonmail.com>
2026-05-08 09:51:54 +0200

    08/05-  acabado fxml (escenas base) + poco lógica necesaria del HelloController

--- UX/TodoTabla/src/main/java/com/decroly/todotabla/HelloController.java
+++ UX/TodoTabla/src/main/java/com/decroly/todotabla/HelloController.java
@@ -1,14 +1,61 @@
 package com.decroly.todotabla;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button addTarea;
+
+    @FXML
+    private Button filterTarea;
+
+    @FXML
+    private Button buscarTarea;
+
+//    @FXML
+//    private void initialize(){
+//
+//    }
+
+    private Stage ventanaSecundaria;
+
+    @FXML
+    private void abrirVentanaSecundaria() {
+        try {
+
+            if(ventanaSecundaria != null && ventanaSecundaria.isShowing()){
+                System.out.println("No se puede volver a abrir, hay una sesion existente");
+                return;
+            }
+
+            // Cargar el archivo FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view2.fxml"));
+            Parent root = loader.load();
+
+            // Crear una nueva ventana (Stage)
+            ventanaSecundaria = new Stage();
+            ventanaSecundaria.setTitle("Añadir tarea");
+            ventanaSecundaria.setScene(new Scene(root));
+
+            ventanaSecundaria.setResizable(false);
+            ventanaSecundaria.setAlwaysOnTop(true);
+
+            // Mostrar la ventana
+            ventanaSecundaria.show();
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
