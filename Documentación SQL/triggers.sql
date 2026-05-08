44b4feb4fd3c0d8a5e0afbdb895c2ed79458120a
COMPUS021\daw1 <mvalienter2501@educantabria.es>
2026-05-08 11:10:13 +0200

    Finalización de consultas y del Word del proyecto, así como su documentación.

--- Documentación SQL/triggers.sql
+++ Documentación SQL/triggers.sql
@@ -1,6 +1,12 @@
 DELIMITER $$
 CREATE TRIGGER guardarHistorial AFTER UPDATE ON tarea FOR EACH ROW 
 BEGIN 
-	INSERT INTO historial_tareas(estado, tarea_id, fecha_cambio) SELECT NEW.estado, NEW.id, NOW();
+	INSERT INTO historial_tareas(estado, tarea_id, fecha_cambio) VALUE (NEW.estado, NEW.id, NOW());
 END $$
 DELIMITER ;
+
+UPDATE tarea SET estado='InProgress' WHERE id=1;
+SELECT t.nombre, t.estado AS 'estado actual', h.* FROM tarea t
+JOIN historial_tareas h ON t.id = h.tarea_id
+WHERE t.id = 1
+ORDER BY h.fecha_cambio;
