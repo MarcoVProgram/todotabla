2a16ad77b79858f1a315ea61dbabde3aa9da8e49
Joaquin Ottonello <joaquinottonello@protonmail.com>
2026-05-08 11:09:11 +0200

    08/05- acabado menu principal básico (con lógica para abrir y cerrar la ventana) (funcional a medias)

--- UX/TodoTabla/src/main/java/com/decroly/todotabla/HelloApplication.java
+++ UX/TodoTabla/src/main/java/com/decroly/todotabla/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.decroly.todotabla;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,10 +11,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1500, 800);
         stage.setTitle("KanBan");
         stage.setScene(scene);
+
+        Platform.setImplicitExit(false); //hacer que no se cierre auto al cerrar escenas
+
         stage.setResizable(false);
         stage.show();
     }
--- UX/TodoTabla/src/main/java/com/decroly/todotabla/HelloController.java
+++ UX/TodoTabla/src/main/java/com/decroly/todotabla/HelloController.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,10 +22,24 @@ public class HelloController {
     @FXML
     private Button buscarTarea;
 
-//    @FXML
-//    private void initialize(){
-//
-//    }
+    @FXML
+    private Button panelKanbanBtn;
+
+    @FXML
+    private ImageView returnBtn;
+
+
+
+    @FXML
+    private void initialize(){
+    }
+
+    @FXML
+    private void cerrarPrincipal(){
+        Stage principal = (Stage) panelKanbanBtn.getScene().getWindow();
+
+        principal.close();
+    }
 
     private Stage ventanaSecundaria;
 
@@ -57,5 +72,44 @@ public class HelloController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private Stage ventanaKanban;
+
+    @FXML
+    private void abrirVentanaMain() {
+        try {
+
+            if(ventanaKanban != null && ventanaKanban.isShowing()){
+                System.out.println("No se puede volver a abrir, hay una sesion existente");
+                return;
+            }
+
+            // Cargar el archivo FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+            Parent root = loader.load();
+
+            // Crear una nueva ventana (Stage)
+            ventanaKanban = new Stage();
+            ventanaKanban.setTitle("KanBan");
+            ventanaKanban.setScene(new Scene(root));
+
+            ventanaKanban.setResizable(false);
+
+            cerrarPrincipal();
+
+            // Mostrar la ventana
+            ventanaKanban.show();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void returnToMain(){
+        Stage principal = (Stage) panelKanbanBtn.getScene().getWindow();
+        principal.show();
     }
 }
