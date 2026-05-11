a8717466e0399c16c0bab450b6e19e09775fa3d8
JoaquinDecroly <joaquinottonello@gmail.com>
2026-05-11 18:03:59 +0200

    11/05- hecho metodo añadir tarea en controller (falta consulta)

--- UX/TodoTabla/src/main/java/com/decroly/todotabla/HelloController.java
+++ UX/TodoTabla/src/main/java/com/decroly/todotabla/HelloController.java
@@ -1,7 +1,11 @@
 package com.decroly.todotabla;
 
+import com.decroly.todotabla.model.Miembro;
 import com.decroly.todotabla.model.Navigator;
-import com.decroly.todotabla.model.Tarea;
+import com.decroly.todotabla.model.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -12,22 +16,36 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloController {
+    //lista miembros
+    List<Miembro> miembros = new ArrayList<>();
+    ObservableList<Miembro> obsMiembros = FXCollections.observableList(miembros);
 
+    //lista tareas
+    List<Tarea> tareas = new ArrayList<>();
+    ObservableList<Tarea> obsTareas = FXCollections.observableList(tareas);
     
-    //pestaña inicio
+    //lista proyectos
+    List<Proyecto> proyectos = new ArrayList<>();
+    ObservableList<Proyecto> obsProyectos = FXCollections.observableList(proyectos);
+    
+    //PESTAÑA INICIO
     @FXML
     private Button panelKanbanBtn;
 
     @FXML
     private Node root;
     
-    //pestaña kanban
+    //PESTAÑA KANBAN
     @FXML
     private ImageView returnBtn;
 
-    //pestaña tarea
+    //PESTAÑA TAREA
     @FXML
     private Button addTarea;
 
@@ -40,27 +58,34 @@ public class HelloController {
     @FXML
     public Label nombreProyecto;
 
-
+    @FXML
     public TextField nombreTarea;
-
+    
+    @FXML
     public ComboBox<String> comboBoxPrioridadTarea;
 
+    @FXML
     public DatePicker fechaAsignacionTarea;
-    
+
+    @FXML
     public DatePicker fechaFinTarea;
 
+    @FXML
     public ListView<Tarea> listViewTareas;
 
-    //variables aux
+    //VARIABLES AUXILIARES
     private Stage ventanaSecundaria;
-
+    
+    
+    //FORMATTER
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     @FXML
     private void initialize(){
     }
 
 
-
+//----------------DESPLAZAMIENTO ENTRE VENTANAS-------------
     @FXML
     private void abrirVentanaTarea() { //panel tarea
         try {
@@ -89,19 +114,36 @@ public class HelloController {
             e.printStackTrace();
         }
     }
-
-
-
     @FXML
     private void abrirVentanaPrincipal() throws IOException { //abrir panel kanban
         Stage stage = (Stage) root.getScene().getWindow();
         Navigator.changeScene(stage, "/com/decroly/todotabla/kanban-view.fxml");
     }
-    
-
     @FXML
     private void returnToMain() throws IOException { //abrir pantalla principal (menú)
         Stage stage = (Stage) returnBtn.getScene().getWindow();
         Navigator.changeScene(stage, "/com/decroly/todotabla/main-view.fxml");
     }
+    
+    //--------AGREGAR TAREA-------------
+    @FXML
+    private boolean addTarea(){
+        boolean added = false;
+
+        //obtener valores campos
+        String nombre = nombreTarea.getText();
+        int prioridad = Integer.parseInt(comboBoxPrioridadTarea.getSelectionModel().getSelectedItem());
+        LocalDate fechaInit = LocalDate.parse(fechaAsignacionTarea.getValue().format(dtf));
+        LocalDate fechaFin = LocalDate.parse(fechaFinTarea.getValue().format(dtf));
+        
+        //valores extra necesarios
+        Miembro idMiembro = null;
+        Proyecto idProyecto = null;
+        
+        Tarea tarea = new Tarea(nombre, idMiembro, prioridad, null, fechaInit, fechaFin, idProyecto);
+        obsTareas.add(tarea);
+
+        return added;
+    }
+
 }
