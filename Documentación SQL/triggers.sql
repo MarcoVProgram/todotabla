7a4b804fba58d98bbfa771c1d14d32549034de3e
COMPUS021\daw1 <mvalienter2501@educantabria.es>
2026-05-12 09:05:25 +0200

    Comienzo TareasDBB y actualización Triggers.

--- Documentación SQL/triggers.sql
+++ Documentación SQL/triggers.sql
@@ -1,9 +1,19 @@
+USE todotabla;
+
 DELIMITER $$
-CREATE TRIGGER guardarHistorial AFTER UPDATE ON tarea FOR EACH ROW 
+CREATE TRIGGER guardarHistorialCambio AFTER UPDATE ON tarea FOR EACH ROW 
 BEGIN 
-	INSERT INTO historial_tareas(estado, tarea_id, fecha_cambio) VALUE (NEW.estado, NEW.id, NOW());
+	INSERT INTO historial_tareas(estado, tarea_id, fecha_cambio) VALUES (NEW.estado, NEW.id, NOW());
 END $$
 DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER guardarHistorialInsercion AFTER INSERT ON tarea FOR EACH ROW 
+BEGIN 
+	INSERT INTO historial_tareas(estado, tarea_id, fecha_cambio) VALUES (NEW.estado, NEW.id, NOW());
+END $$
+DELIMITER ;
+
 
 UPDATE tarea SET estado='InProgress' WHERE id=1;
 SELECT t.nombre, t.estado AS 'estado actual', h.* FROM tarea t
