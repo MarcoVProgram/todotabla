44854a035d4f93d533e58ee1c45ed6474156c7bb
JoaquinDecroly <joaquinottonello@gmail.com>
2026-05-21 19:25:46 +0200

    no message
    (cherry picked from commit 5b09d948049e15e246e31ede2261de60e4abbbc2)

--- UX/TodoTabla/src/main/java/com/decroly/todotabla/HelloController.java
+++ UX/TodoTabla/src/main/java/com/decroly/todotabla/HelloController.java
@@ -256,7 +256,7 @@ public class HelloController implements Initializable {
         int contActivos = 0;
 
         for(Proyecto p :obsProyectoList){
-                if(p.getFechaCierre() != null){
+                if(p.getFechaCierre() == null){
                     contActivos++;
                 }
             }
@@ -268,7 +268,7 @@ public class HelloController implements Initializable {
         int contArchivados = 0;
 
         for(Proyecto p :obsProyectoList) {
-            if (p.getFechaCierre() == null) {
+            if (p.getFechaCierre() != null) {
                 contArchivados++;
             }
         }
