9ab1accd9ef206e5f581802d1d0191261f854e17
COMPUS021\daw1 <mvalienter2501@educantabria.es>
2026-05-21 09:51:49 +0200

    Comeinzo de movimiento de tareas por ratón

--- UX/TodoTabla/src/main/java/module-info.java
+++ UX/TodoTabla/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.decroly.todotabla {
     requires javafx.fxml;
     requires java.sql;
     requires jdk.jdi;
+    requires java.desktop;
 
 
     opens com.decroly.todotabla to javafx.fxml;
