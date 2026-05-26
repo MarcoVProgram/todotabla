1253c14c9e98f67fdad27495a2aaa76826d84e1b
Joaquin Ottonello <joaquinottonello@protonmail.com>
2026-05-26 09:51:09 +0200

    implementación base ventana usuarios con controlador (falta botón para añadir usuario)

--- UX/TodoTabla/src/main/java/com/decroly/todotabla/ProyectoController.java
+++ UX/TodoTabla/src/main/java/com/decroly/todotabla/ProyectoController.java
@@ -1,15 +1,27 @@
 package com.decroly.todotabla;
 
+import com.decroly.todotabla.model.Estado;
 import com.decroly.todotabla.model.Proyecto;
+import com.decroly.todotabla.model.Tarea;
+import com.decroly.todotabla.model.Usuario;
 import com.decroly.todotabla.model.sql.ProyetosBDD;
 
+import com.decroly.todotabla.model.sql.TareasBDD;
+import com.decroly.todotabla.model.sql.UsuariosBDD;
+import com.decroly.todotabla.utils.EstadoPrograma;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.chrono.IsoChronology;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class ProyectoController implements Initializable{
@@ -19,6 +31,12 @@ public class ProyectoController implements Initializable{
 
     @FXML
     private DatePicker fechaProyecto;
+
+    private static Stage ventanaSecundaria;
+
+    public static Stage getVentanaSecundaria() {
+        return ventanaSecundaria;
+    }
 
 //    @FXML
 //    private Button addButtom;
@@ -46,5 +64,43 @@ public class ProyectoController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         fechaProyecto.setValue(LocalDate.now());
+    }
+
+    @FXML
+    private void abrirVentanaUsuarios() { //panel usuarios
+        try {
+
+            if(ventanaSecundaria != null && ventanaSecundaria.isShowing()){
+                System.out.println("No se puede volver a abrir, hay una sesion existente");
+                return;
+            }
+
+            // Cargar el archivo FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("usuarios-form.fxml"));
+            Parent root = loader.load();
+
+            // Crear una nueva ventana (Stage)
+            ventanaSecundaria = new Stage();
+            ventanaSecundaria.setTitle("Añadir usuarios");
+            ventanaSecundaria.setScene(new Scene(root));
+
+            ventanaSecundaria.setResizable(false);
+
+            if(ventanaSecundaria.isFocused()){
+                ventanaSecundaria.setAlwaysOnTop(true);
+            }else{
+                ventanaSecundaria.setAlwaysOnTop(false);
+            }
+
+//            listViewTareas.setItems(obsTareas);
+
+            // Mostrar la ventana
+            ventanaSecundaria.showAndWait();
+
+            // TODO Hay que refrescar las listas del Kanban controller
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- UX/TodoTabla/src/main/java/com/decroly/todotabla/UsuariosController.java
+++ UX/TodoTabla/src/main/java/com/decroly/todotabla/UsuariosController.java
@@ -1,4 +1,119 @@
 package com.decroly.todotabla;
 
-public class UsuariosController {
+import com.decroly.todotabla.model.Proyecto;
+import com.decroly.todotabla.model.Tarea;
+import com.decroly.todotabla.model.Usuario;
+import com.decroly.todotabla.model.sql.TareasBDD;
+import com.decroly.todotabla.model.sql.UsuariosBDD;
+import com.decroly.todotabla.utils.EstadoPrograma;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class UsuariosController implements Initializable {
+    @FXML
+    public ListView<Usuario> listViewUsuarios;
+    List<Usuario> usuarioList = new ArrayList<>();
+    ObservableList<Usuario> obsUsuarioList = FXCollections.observableList(usuarioList);
+
+    @FXML
+    public TextField buscarUsuario;
+
+    private static Stage ventanaSecundaria;
+
+    public static Stage getVentanaSecundaria() {
+        return ventanaSecundaria;
+    }
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        obsUsuarioList.addAll(UsuariosBDD.getUsuarios().values());
+        listViewUsuarios.setItems(obsUsuarioList);
+
+        actualizarUsuarios();
+
+        listViewUsuarios.setCellFactory(usuarioList -> new ListCell<Usuario>() {
+
+            @Override
+            protected void updateItem(Usuario user, boolean empty) {
+                super.updateItem(user, empty);
+
+                if (empty || user == null) {
+
+                    setGraphic(null);
+
+                } else {
+
+                    // Título
+                    Label titulo = new Label(user.getNombre());
+                    Label subTitulo = new Label(user.getApellidos());
+
+                    titulo.getStyleClass().add("titulo-tarea");
+                    subTitulo.getStyleClass().add("subTitulo-tarea");
+
+                    // Fecha fin
+                    Label email = new Label(user.getEmail());
+
+                    email.getStyleClass().add("subTitulo2-tarea");
+
+                    // Card completa
+                    VBox card = new VBox(10, titulo, subTitulo, email);
+
+                    card.getStyleClass().add("task-card");
+
+                    setGraphic(card);
+                }
+            }
+        });
+
+        listViewUsuarios.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+//        listViewUsuarios.setOnMouseClicked(event -> {
+//            if(event.getButton() == MouseButton.SECONDARY || event.getClickCount() == 2){
+//                try {
+//                    abrirVentanaPrincipal();
+//
+//                } catch (IOException e) {
+//                    showAlert("Ocurrió un error inesperado y no se puede acceder al proyecto", "Cagaste");
+//                }
+//            }
+//        });
+    }
+
+
+    private void actualizarUsuarios() {
+        Map<Integer, Usuario> todosUsuarios = UsuariosBDD.getUsuarios();
+        if (todosUsuarios != null) {
+            listViewUsuarios.refresh();
+        }
+    }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+
+
+
 }
