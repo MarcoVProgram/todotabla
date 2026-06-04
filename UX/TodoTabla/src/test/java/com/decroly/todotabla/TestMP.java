76a397ed8334e530883dbc9122ef9ad210545978
COMPU-S028\daw1 <example>
2026-06-04 09:20:39 +0200

    Test empezado en clase

--- UX/TodoTabla/src/test/java/com/decroly/todotabla/TestMP.java
+++ UX/TodoTabla/src/test/java/com/decroly/todotabla/TestMP.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Connection;
 
+import com.decroly.todotabla.model.Integrante;
+import com.decroly.todotabla.model.sql.IntegrantesBDD;
 import org.junit.jupiter.api.*;
 
 import com.decroly.todotabla.*;
@@ -14,7 +16,15 @@ public class TestMP {
     public static boolean main(){
         boolean estado = false;
 
-        // TODO MARIO tiene que comprobar algo aqui. Repito MARIO
+        try {
+            for (Object i : IntegrantesBDD.getIntegrantes().values()) {
+                assertNotNull(i, "Los datos van mal");
+                // assertEquals(i.getClass(), Integrante.class);
+            }
+            estado = true;
+        } catch (Exception e) {
+            return false;
+        }
 
         return estado;
     }
